a364c4f5db416f3df3bf5f0107d2408f5d0ad5fb
Popovkov57 <kevin.patrzek@live.fr>
2022-09-27 19:36:02 +0200

    Add pagination

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -10,11 +10,14 @@ import { ProductRepository } from "../model/product.repository";
 export class StoreComponent {
 
     public selectedCategory: string = null;
+    public productsPerPage = 4;
+    public selectedPage = 1;
 
     constructor(private repository: ProductRepository) {}
 
     get products(): Product[] {
-        return this.repository.getProducts(this.selectedCategory);
+        let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+        return this.repository.getProducts(this.selectedCategory).slice(pageIndex, pageIndex + this.productsPerPage);
     }
 
     get categories(): string[] {
@@ -23,6 +26,19 @@ export class StoreComponent {
 
     changeCategory(newCategory?: string) {
         this.selectedCategory = newCategory;
+    }
+
+    changePage(newPage: number) {
+        this.selectedPage = newPage;
+    }
+
+    changePageSize(newSize: number) {
+        this.productsPerPage = newSize;
+        this.changePage(1);
+    }
+
+    get pageNumbers(): number[] {
+        return Array(Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage)).fill(0).map((x,i) => i+1);
     }
 
 }
