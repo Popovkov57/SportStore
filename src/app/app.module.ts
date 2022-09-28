4ff93ac3df8dbc32484428dd086d015e07f198ed
Popovkov57 <kevin.patrzek@live.fr>
2022-09-28 12:09:19 +0200

    Add detail and checkout component and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from './store/store.module';
-
 import { AppComponent } from './app.component';
+import { StoreComponent } from './store/store.component';
+import { CheckoutComponent } from './store/checkout.component';
+import { CartDetailComponent } from './store/cartDetail.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
+  imports: [BrowserModule, StoreModule,
+    RouterModule.forRoot([
+      { path: "store", component: StoreComponent },
+      { path: "cart", component: CartDetailComponent },
+      { path: "checkout", component: CheckoutComponent },
+      { path: "**", redirectTo: "/store" }
+    ])],
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule, StoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
