c1ad0150892006dbc9288fb894fa31f6f1f72e40
Popovkov57 <kevin.patrzek@live.fr>
2022-09-30 17:19:15 +0200

    Responding to connectivity changes

--- src/app/store/cartDetail.component.ts
+++ src/app/store/cartDetail.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Cart } from "../model/cart.model";
+import { ConnectionService } from "../model/connection.service";
 
 @Component({
     templateUrl: "cartDetail.component.html"
@@ -7,6 +8,11 @@ import { Cart } from "../model/cart.model";
 
 export class CartDetailComponent {
 
-    constructor(public cart: Cart) {}
+    public connected: boolean = true;
+
+    constructor(public cart: Cart, private connection: ConnectionService) {
+        this.connected = this.connection.connected;
+        connection.Changes.subscribe((state) => this.connected = state);
+    }
 
 }
